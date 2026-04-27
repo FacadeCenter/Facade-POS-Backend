054ddc7ab7aeb05d553a35bc36c0bfa2e02eeaca
AsithaLKonara <asithalakmalkonara11992081@gmail.com>
2026-04-27 23:01:31 +0530

    fix(deploy): simplify prisma config and set node engine

--- src/prisma/prisma.config.ts
+++ src/prisma/prisma.config.ts
@@ -1,12 +1,3 @@
 import { PrismaClient } from '@prisma/client';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
-
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL as string,
-    },
-  },
-});
+export const prisma = new PrismaClient();
